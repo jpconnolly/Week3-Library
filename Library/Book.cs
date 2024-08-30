04b19e876c3c5dbcf15bc437861375cb2a4877ef
James Connolly <jp.connolly@ulster.ac.uk>
2024-08-30 17:37:19 +0100

    Add project files.

---
+++ Library/Book.cs
@@ -0,0 +1,44 @@
+﻿namespace Library
+{
+    class Book
+    {
+        private string title; // private variable
+        private string author;// private variable
+        private string isbn; // private variable
+
+        // Title property allows access
+        // to the title private variable
+        public string Title
+        {
+            get { return title; }  // get method
+            set { title = value; } // set method
+        }
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+        public string ISBN
+        {
+            get { return isbn; }
+            set { isbn = value; }
+        }
+
+        // Constructor to add a new book
+        public Book(string bookTitle, string bookAuthor, string bookISBN)
+        {
+            this.title = bookTitle;
+            this.author = bookAuthor;
+            this.isbn = bookISBN;
+        }
+
+        // Method to display information about a book
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Book title: {Title}");
+            Console.WriteLine($"Book Author: {Author}");
+            Console.WriteLine($"Book ISBN: {ISBN}");
+            Console.WriteLine();
+        }
+    }
+}
